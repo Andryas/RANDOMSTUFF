e8372d3e9ce4b90474db304871a513c1285af168
Andryas <Andryaas@gmail.com>
2019-07-06 18:58:54 -0300

    add keywords highlight e atom pkgs

---
+++ atom/pkg.sh
@@ -0,0 +1,7 @@
+# Install packages R
+Rscript -e 'install.packages(c("languageserver", "formatR", "IRkernel"))'
+
+# Install packages Atom
+sudo apm install ide-r
+sudo apm install atom-beautify
+sudo apm install atom-language-r
