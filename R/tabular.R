09837087be214762042054e7d089f9f63e904b30
Andryas <Andryaas@gmail.com>
2019-08-24 19:34:23 -0300

    add tabular.R

---
+++ R/tabular.R
@@ -0,0 +1,13 @@
+tabular <- function(df, ...) {
+  stopifnot(is.data.frame(df))
+
+  align <- function(x) if (is.numeric(x)) "r" else "l"
+  col_align <- vapply(df, align, character(1))
+
+  cols <- lapply(df, format, ...)
+  contents <- do.call("paste",
+    c(cols, list(sep = " \\tab ", collapse = "\\cr\n  ")))
+
+  paste("\\tabular{", paste(col_align, collapse = ""), "}{\n  ",
+    contents, "\n}\n", sep = "")
+}
