d669d272caeed99e09de1b4c0126d469aa7303b8
Andryas Waurzenczak <andryaswaurzenczak@pop-os.localdomain>
2021-04-14 07:45:48 -0300

    add user conf

---
+++ sh/popos.sh
@@ -0,0 +1,36 @@
+# Git
+sudo apt install git -y
+
+# Gparted
+sudo apt install gparted
+
+# Docker
+sudo apt install docker.io
+
+# htop
+sudo apt install htop -y
+
+# gdebi
+sudo apt install gdebi -y
+
+# Libs R
+sudo apt install libfontconfig1-dev libxml2-dev libssl-dev libcurl4-openssl-dev libmariadbclient-dev libgsl0-dev libharfbuzz-dev -y
+
+# pip3
+sudo apt install python3-pip
+
+# vpn
+sudo apt install openfortivpn -y
+
+# Install Steam
+sudo apt-get install steam -y
+
+# mirror tablet/android
+sudo snap install scrcpyscr
+
+# mv config  pop.os
+sudo mv user ~/.config/dconf/user 
+
+# Finish
+sudo apt update
+sudo apt upgrade -y
