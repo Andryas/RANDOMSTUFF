a176b78ed1de88ac9e6047a13fdb8addc6cdcc87
Andryas Wavrzenczak <aw@pop-os.localdomain>
2022-01-04 00:48:19 -0300

    update pop os sh

--- sh/popos.sh
+++ sh/popos.sh
@@ -13,11 +13,8 @@ sudo apt install htop -y
 # gdebi
 sudo apt install gdebi -y
 
-# Libs R
-sudo apt install libfontconfig1-dev libxml2-dev libssl-dev libcurl4-openssl-dev libmariadbclient-dev libgsl0-dev libharfbuzz-dev -y
-
-# pip3
-sudo apt install python3-pip
+# # pip3
+# sudo apt install python3-pip
 
 # vpn
 sudo apt install openfortivpn -y
@@ -25,12 +22,30 @@ sudo apt install openfortivpn -y
 # Install Steam
 sudo apt-get install steam -y
 
-# mirror tablet/android
-sudo snap install scrcpyscr
+# # mirror tablet/android
+# sudo snap install scrcpyscr
+
+# Java
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+
+# Libs R
+# https://www.tecmint.com/pyenv-install-and-manage-multiple-python-versions-in-linux/
+sudo apt install curl git-core gcc make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev
+sudo apt install libfontconfig1-dev libxml2-dev libssl-dev libcurl4-openssl-dev libbz2-dev libmariadbclient libgsl0-dev libharfbuzz-dev -y
+sudo apt-get install build-essential
+sudo apt-get install fort77
+sudo apt-get install xorg-dev
+sudo apt-get install liblzma-dev  libblas-dev gfortran
+sudo apt-get install gcc-multilib
+sudo apt-get install gobjc++
+sudo apt-get install aptitude
+sudo aptitude install libreadline-dev
 
 # mv config  pop.os
-sudo mv user ~/.config/dconf/user 
+# sudo mv user ~/.config/dconf/user 
 
 # Finish
 sudo apt update
 sudo apt upgrade -y
+
