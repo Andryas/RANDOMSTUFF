550449cb2c53ce12fb5eeb91bc34bdc3209ddaf8
Andryas Wavrzenczak <andryaas@gmail.com>
2022-03-07 13:21:23 -0500

    add format_num

--- R/format_num.R
+++ R/format_num.R
@@ -11,43 +11,42 @@
 #'
 #' library(ggplot2)
 #' ggplot(cars, aes(x = dist, y = speed)) +
-#'  geom_point() +
-#'  scale_y_continuous(label = format_num)
+#'     geom_point() +
+#'     scale_y_continuous(label = format_num)
 #'
 #' format_num(seq(100000, 1000000, 100000))
-#'
 #' @export
 format_num <- function(x = NULL, pre = "", pos = "", signif = 1) {
-  sapply(x, function(x) {
-    if (is.infinite(x)) {
-      return(x)
-    } else if (!is.na(x)) {
-      tn <- round(abs(x) / 1e12, signif)
-      b <- round(abs(x) / 1e9, signif)
-      m <- round(abs(x) / 1e6, signif)
-      k <- round(abs(x) / 1e3, signif)
+    sapply(x, function(x) {
+        if (is.infinite(x)) {
+            return(x)
+        } else if (!is.na(x)) {
+            tn <- round(abs(x) / 1e12, signif)
+            b <- round(abs(x) / 1e9, signif)
+            m <- round(abs(x) / 1e6, signif)
+            k <- round(abs(x) / 1e3, signif)
 
-      if (x >= 0) {
-        x_is_positive <- ""
-      } else {
-        x_is_positive <- "-"
-      }
+            if (x >= 0) {
+                x_is_positive <- ""
+            } else {
+                x_is_positive <- "-"
+            }
 
-      if (k < 1) {
-        r <- paste0(x_is_positive, pre, round(abs(x), signif))
-      } else if (m < 1) {
-        r <- paste0(x_is_positive, pre, k, "k")
-      } else if (b < 1) {
-        r <- paste0(x_is_positive, pre, m, "m")
-      } else if (tn < 1) {
-        r <- paste0(x_is_positive, pre, b, "bn")
-      } else {
-        r <- paste0(x_is_positive, pre, scales::comma(tn), "tn")
-      }
+            if (k < 1) {
+                r <- paste0(x_is_positive, pre, round(abs(x), signif))
+            } else if (m < 1) {
+                r <- paste0(x_is_positive, pre, k, "k")
+            } else if (b < 1) {
+                r <- paste0(x_is_positive, pre, m, "m")
+            } else if (tn < 1) {
+                r <- paste0(x_is_positive, pre, b, "bn")
+            } else {
+                r <- paste0(x_is_positive, pre, scales::comma(tn), "tn")
+            }
 
-      return(paste0(r, pos))
-    } else if (is.na(x) | is.null(x)) {
-      return("-")
-    }
-  })
+            return(paste0(r, pos))
+        } else if (is.na(x) | is.null(x)) {
+            return("-")
+        }
+    })
 }
